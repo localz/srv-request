48704ba415e7c39f0bfef3bb49dd891037dfbcd4
Terence Huynh <terence.huynh@localz.co>
2017-08-16 13:40:17 +1000

    Add tests

---
+++ test/helpers/Constants.js
@@ -0,0 +1,6 @@
+exports.TEST_NAME = 'company.localz'
+exports.TEST_PORT = 80
+exports.TEST_URL = `http://${exports.TEST_NAME}`
+exports.SRV_NAME = 'fake.srv.dns.link'
+exports.SRV_PORT = 9001
+exports.SRV_URL = `http://${exports.SRV_NAME}:${exports.SRV_PORT}`
